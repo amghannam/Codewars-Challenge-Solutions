83f1a990ceb53f7a2ede96e708b26d518b5135d5
Ahmed Ghannam <aghannam@outlook.com>
2016-09-05 01:05:59 -0500

    Update jadenCase.java

--- src/jadenCase.java
+++ src/jadenCase.java
@@ -1,10 +1,10 @@
 /*
-Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
-Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, 
-he is known for almost always capitalizing every word.
+* Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+* Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, 
+* he is known for almost always capitalizing every word.
 
-Your task is to convert strings to how they would be written by Jaden Smith. Note that the Java version expects 
-a return value of null for an empty string or null.
+* Your task is to convert strings to how they would be written by Jaden Smith. Note that the Java version expects 
+* a return value of null for an empty string or null.
 */ 
 
 public class JadenCase {
