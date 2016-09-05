8706b8f17b6b70ac3c2d1c66324504450e970d8f
Ahmed Ghannam <aghannam@outlook.com>
2016-09-05 00:59:18 -0500

    Create jadenCase.java

---
+++ src/jadenCase.java
@@ -0,0 +1,32 @@
+/*
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, 
+he is known for almost always capitalizing every word.
+
+Your task is to convert strings to how they would be written by Jaden Smith. 
+*/ 
+
+public class JadenCase {
+
+
+	public static String toJadenCase(String str) {
+		String result = "";
+		String capitalized = "";
+		if (str == null || str.isEmpty())
+			return null;
+		// First, split the words in the given string by space and store them in
+		// an array.
+		String[] words = str.split("\\s+");
+		for (int i = 0; i < words.length; i++) {
+			// Second, capitalize each word in the array.
+			capitalized = words[i].substring(0, 1).toUpperCase();
+			// Finally, concatenate the capitalized word with the result.
+			if (i == words.length - 1) {
+				result += capitalized + words[i].substring(1, words[i].length());
+			} else {
+				result += capitalized + words[i].substring(1, words[i].length()) + " ";
+			}
+		}
+		return result;
+	}
+	}
