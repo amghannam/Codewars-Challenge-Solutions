e5ebce8607529b58b7f7a21006c8fa8ca6c6c01c
Ahmed Ghannam <aghannam@outlook.com>
2016-09-05 01:05:10 -0500

    Update minMax.java

--- src/minMax.java
+++ src/minMax.java
@@ -1,14 +1,16 @@
 /* 
-Story
 
-Ben has a very simple idea to make some profit: he buys something and sells it again. 
-Of course, this wouldn't give him any profit at all if he was simply to buy and sell 
-it at the same price. Instead, he's going to buy it for the lowest possible price and 
-sell it at the highest.
+*** Story***
 
-Task
+* Ben has a very simple idea to make some profit: he buys something and sells it again. 
+* Of course, this wouldn't give him any profit at all if he was simply to buy and sell 
+* it at the same price. Instead, he's going to buy it for the lowest possible price and 
+* sell it at the highest.
 
-Write a function that returns both the minimum and maximum number of the given list/array. 
+*** Task ****
+
+* Write a function that returns both the minimum and maximum number of the given list/array. 
+
 */ 
 
 import java.util.Arrays; 
